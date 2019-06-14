a38bb5fe11eedaa2e55dcbe3d18df5607c079d16
Qianqian Fang <fangqq@gmail.com>
2019-06-14 13:18:42 -0400

    minor update to the basic example

--- samples/basic/create_mouse_jnii.m
+++ samples/basic/create_mouse_jnii.m
@@ -2,10 +2,10 @@ load mouse_head.mat
 bvol=uint8(volimage);
 jnii=jnifticreate(uint8(bvol),'Name','Mouse Head','Description','Binary mask of a mouse-head scan');
 
+savejnifti(jnii,'mousehead.jnii');
+savejnifti(jnii,'mousehead_zlib.jnii','compression','zlib');
 savejnifti(jnii,'mousehead_gzip.bnii','compression','gzip');
-savejnifti(jnii,'mousehead.jnii');
 savejnifti(jnii,'mousehead.bnii');
-savejnifti(jnii,'mousehead.jnii');
 if(exist('zmat')==3)
    savejnifti(jnii,'mousehead_lzma.jnii','compression','lzma');
 end
